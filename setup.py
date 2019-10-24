280e2493398122ca6536566b261aa3febeaac447
Dema Ushchapovskyy <dema.ushchapovskyy@gmail.com>
2019-10-24 14:26:55 +0100

    setuptools data fix

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(name='gym-crypto',
-      packages=['gym_crypto'],
-      version='0.0.2',
+      packages=find_packages(exclude='tests'),
+      include_package_data=True,
+      version='0.0.3',
       licence='MIT',
       url='https://github.com/dema-u/gym-crypto',
       description='Reinforcement Learning OpenAI Gym environment for crypto-currency trading.',
